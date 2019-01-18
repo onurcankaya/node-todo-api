aa8da149eb5c11262fd859779e360329f2ed8e58
onurcankaya <onur.eren.cankaya@gmail.com>
2019-01-18 16:23:52 +0100

    added POST /todos route and refactored mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,20 @@
+const { mongoose } = require('../db/mongoose')
+
+const Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number
+  }
+})
+
+module.exports = { Todo }
