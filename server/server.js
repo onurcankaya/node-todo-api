869a951e8b9524f85ee51d4d6bdbf908912cee83
onurcankaya <onur.eren.cankaya@gmail.com>
2019-01-19 13:42:32 +0100

    added GET /todos route

--- server/server.js
+++ server/server.js
@@ -8,16 +8,28 @@ const app = express()
 
 app.use(bodyParser.json())
 
+// create a todo
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text,
     completed: 'false'
   })
-  
+
   todo.save().then((doc) => {
     res.send(doc)
   }).catch((err) => {
     res.status(400).send(err)
+  })
+})
+
+// get all todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    })
+  }).catch((e) => {
+    res.status(400).send(e)
   })
 })
 
