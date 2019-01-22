d1044820d344252410bd27ea123c41c83fa29c10
onurcankaya <onur.eren.cankaya@gmail.com>
2019-01-22 09:44:35 +0100

    added PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ObjectID } = require('mongodb')
@@ -67,7 +68,33 @@ app.delete('/todos/:id', (req, res) => {
     }
     res.status(200).send(todo)
   }).catch((error) => {
-    res.status(400).send()
+    res.status(400).send(error)
+  })
+})
+
+// update a todo
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id
+  const body = _.pick(req.body, ['text', 'completed'])
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(id, { $set : body }, { new: true }).then((todo) => {
+    if (!todo) {
+      res.status(404).send()
+    }
+    res.status(200).send({ todo })
+  }).catch((e) => {
+    res.status(404).send(e)
   })
 })
 
